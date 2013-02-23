623e1098ed18c1018921e10beaeb1cc06a3482e3
Gleiton <gleitonfranco@aluno.fanor.edu.br>
2013-02-23 19:09:15 -0300

    Update src/main/br/com/caelum/leilao/servico/Avaliador.java

--- src/main/br/com/caelum/leilao/servico/Avaliador.java
+++ src/main/br/com/caelum/leilao/servico/Avaliador.java
@@ -4,7 +4,7 @@ import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
 class Avaliador {   
-
+//
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY; 
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
 	private double media = 0;
